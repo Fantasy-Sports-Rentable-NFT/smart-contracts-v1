97b0b729b382deae1f502165c68e77f3e701870a
Jamie Pickett <jpickett713@gmail.com>
2022-10-26 21:32:27 -0400

    added interface and mint and constructor args

--- contracts/RentablePlayerNFT.sol
+++ contracts/RentablePlayerNFT.sol
@@ -3,43 +3,76 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./interfaces/IERC4907.sol";
+import "./interfaces/IOverCollateralizedAuction.sol";
+
+error InvalidCaller();
+error PlayerMaxCap(uint256 capAmount);
 
 contract ERC4907 is ERC721, IERC4907 {
-    struct UserInfo
-    {
-        address user;   // address of user role
+    address admin;
+    IOverCollateralizedAuction public auction;
+    uint256 public currTokenId;
+
+    struct UserInfo {
+        address user; // address of user role
         uint64 expires; // unix timestamp, user expires
     }
 
-    mapping (uint256  => UserInfo) internal _users;
+    struct PlayerData {
+        uint32 countMinted; // current number of this player minted
+        uint32 maxMintable; // maximum amount of cards minted for this player
+        uint16[] percentStarted; // out of 1000 (e.g. 807 = 80.7%)
+        uint16[] percentOwned; // out of 1000 (e.g. 807 = 80.7%)
+        /**
+         * price of the auctions first 216 bits,
+         * timestamp ended 32 bits,
+         * type (0 for buy 1 for rent) 1 bit,
+         * maybe add later other info/flags
+         */
+        uint256[] auctionResults;
+    }
 
-    constructor(string memory name_, string memory symbol_)
-     ERC721(name_,symbol_)
-     {
-     }
+    mapping(uint256 => UserInfo) internal _users; // tokenId => UserInfo
+
+    mapping(string => PlayerData) internal players; // player uuid => Player Info
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address _auctionAddr
+    ) ERC721(name_, symbol_) {
+        admin = msg.sender;
+        auction = IOverCollateralizedAuction(_auctionAddr);
+    }
 
     /// @notice set the user and expires of a NFT
     /// @dev The zero address indicates there is no user
     /// Throws if `tokenId` is not valid NFT
     /// @param user  The new user of the NFT
     /// @param expires  UNIX timestamp, The new user could use the NFT before expires
-    function setUser(uint256 tokenId, address user, uint64 expires) public virtual{
-        require(_isApprovedOrOwner(msg.sender, tokenId),"ERC721: transfer caller is not owner nor approved");
-        UserInfo storage info =  _users[tokenId];
+    function setUser(
+        uint256 tokenId,
+        address user,
+        uint64 expires
+    ) public virtual {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        UserInfo storage info = _users[tokenId];
         info.user = user;
         info.expires = expires;
-        emit UpdateUser(tokenId,user,expires);
+        emit UpdateUser(tokenId, user, expires);
     }
 
     /// @notice Get the user address of an NFT
     /// @dev The zero address indicates that there is no user or the user is expired
     /// @param tokenId The NFT to get the user address for
     /// @return The user address for this NFT
-    function userOf(uint256 tokenId)public view virtual returns(address){
-        if( uint256(_users[tokenId].expires) >=  block.timestamp){
-            return  _users[tokenId].user;
-        }
-        else{
+    function userOf(uint256 tokenId) public view virtual returns (address) {
+        if (uint256(_users[tokenId].expires) >= block.timestamp) {
+            return _users[tokenId].user;
+        } else {
             return address(0);
         }
     }
@@ -48,25 +81,46 @@ contract ERC4907 is ERC721, IERC4907 {
     /// @dev The zero value indicates that there is no user
     /// @param tokenId The NFT to get the user expires for
     /// @return The user expires for this NFT
-    function userExpires(uint256 tokenId) public view virtual returns(uint256){
+    function userExpires(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
         return _users[tokenId].expires;
     }
 
     /// @dev See {IERC165-supportsInterface}.
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IERC4907).interfaceId || super.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC4907).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId
-    ) internal virtual override{
+    ) internal virtual override {
         super._beforeTokenTransfer(from, to, tokenId);
 
         if (from != to && _users[tokenId].user != address(0)) {
             delete _users[tokenId];
             emit UpdateUser(tokenId, address(0), 0);
         }
+    }
+
+    function mint(string calldata identifier) external {
+        if (msg.sender != admin) revert InvalidCaller();
+        if (players[identifier].maxMintable <= players[identifier].countMinted)
+            revert PlayerMaxCap(players[identifier].maxMintable);
+        _mint(msg.sender, ++currTokenId);
+        players[identifier].countMinted++;
     }
 }
--- contracts/sealed-bid/OverCollateralizedAuction.sol
+++ contracts/sealed-bid/OverCollateralizedAuction.sol
@@ -5,125 +5,36 @@ import "../solmate/tokens/ERC721.sol";
 import "../solmate/utils/ReentrancyGuard.sol";
 import "../solmate/utils/SafeTransferLib.sol";
 import "./IOverCollateralizedAuctionErrors.sol";
+import "../interfaces/IOverCollateralizedAuction.sol";
 
 /// @title An on-chain, over-collateralization, sealed-bid, second-price auction
-contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, ReentrancyGuard {
+contract OverCollateralizedAuction is
+    IOverCollateralizedAuctionErrors,
+    ReentrancyGuard,
+    IOverCollateralizedAuction
+{
     using SafeTransferLib for address;
-
-    /// @dev Representation of an auction in storage. Occupies three slots.
-    /// @param seller The address selling the auctioned asset.
-    /// @param startTime The unix timestamp at which bidding can start.
-    /// @param endOfBiddingPeriod The unix timestamp after which bids can no
-    ///        longer be placed.
-    /// @param endOfRevealPeriod The unix timestamp after which commitments can
-    ///        no longer be opened.
-    /// @param numUnrevealedBids The number of bid commitments that have not
-    ///        yet been opened.
-    /// @param highestBid The value of the highest bid revealed so far, or 
-    ///        the reserve price if no bids have exceeded it.
-    /// @param secondHighestBid The value of the second-highest bid revealed
-    ///        so far, or the reserve price if no two bids have exceeded it.
-    /// @param highestBidder The bidder that placed the highest bid.
-    /// @param index Auctions selling the same asset (i.e. tokenContract-tokenId
-    ///        pair) share the same storage. This value is incremented for 
-    ///        each new auction of a particular asset.
-    struct Auction {
-        address seller;
-        uint32 startTime;
-        uint32 endOfBiddingPeriod;
-        uint32 endOfRevealPeriod;
-        // =====================
-        uint64 numUnrevealedBids;
-        uint96 highestBid;
-        uint96 secondHighestBid;
-        // =====================
-        address highestBidder;
-        uint64 index;
-    }
-
-    /// @dev Representation of a bid in storage. Occupies one slot.
-    /// @param commitment The hash commitment of a bid value.
-    /// @param collateral The amount of collateral backing the bid.
-    struct Bid {
-        bytes20 commitment;
-        uint96 collateral;
-    }
-
-    /// @notice Emitted when an auction is created.
-    /// @param tokenContract The address of the ERC721 contract for the asset
-    ///        being auctioned.
-    /// @param tokenId The ERC721 token ID of the asset being auctioned.
-    /// @param seller The address selling the auctioned asset.
-    /// @param startTime The unix timestamp at which bidding can start.
-    /// @param bidPeriod The duration of the bidding period, in seconds.
-    /// @param revealPeriod The duration of the commitment reveal period, 
-    ///        in seconds.
-    /// @param reservePrice The minimum price that the asset will be sold for.
-    ///        If no bids exceed this price, the asset is returned to `seller`.
-    event AuctionCreated(
-        address tokenContract,
-        uint256 tokenId,
-        address seller,
-        uint32 startTime,
-        uint32 bidPeriod,
-        uint32 revealPeriod,
-        uint96 reservePrice
-    );
-
-    /// @notice Emitted when a bid commitment is opened.
-    /// @param tokenContract The address of the ERC721 contract for the asset
-    ///        being auctioned.
-    /// @param tokenId The ERC721 token ID of the asset being auctioned.
-    /// @param commitment The commitment that was opened.
-    /// @param bidder The bidder whose bid was revealed.
-    /// @param nonce The random input used to obfuscate the commitment.
-    /// @param bidValue The value of the bid.
-    event BidRevealed(
-        address tokenContract,
-        uint256 tokenId,
-        bytes20 commitment,
-        address bidder,
-        bytes32 nonce,
-        uint96 bidValue
-    );
 
     /// @notice A mapping storing auction parameters and state, indexed by
     ///         the ERC721 contract address and token ID of the asset being
     ///         auctioned.
     mapping(address => mapping(uint256 => Auction)) public auctions;
 
-    /// @notice A mapping storing bid commitments and records of collateral, 
-    ///         indexed by: ERC721 contract address, token ID, auction index, 
+    /// @notice A mapping storing bid commitments and records of collateral,
+    ///         indexed by: ERC721 contract address, token ID, auction index,
     ///         and bidder address. If the commitment is `bytes20(0)`, either
     ///         no commitment was made or the commitment was opened.
-    mapping(address // ERC721 token contract
-        => mapping(uint256 // ERC721 token ID
-            => mapping(uint64 // Auction index
-                => mapping(address // Bidder
-                    => Bid)))) public bids;
+    mapping(address => mapping(uint256 => mapping(uint64 => mapping(address => Bid)))) // ERC721 token contract // ERC721 token ID // Auction index // Bidder
+        public bids;
 
-    /// @notice Creates an auction for the given ERC721 asset with the given
-    ///         auction parameters.
-    /// @param tokenContract The address of the ERC721 contract for the asset
-    ///        being auctioned.
-    /// @param tokenId The ERC721 token ID of the asset being auctioned.
-    /// @param startTime The unix timestamp at which bidding can start.
-    /// @param bidPeriod The duration of the bidding period, in seconds.
-    /// @param revealPeriod The duration of the commitment reveal period, 
-    ///        in seconds.
-    /// @param reservePrice The minimum price that the asset will be sold for.
-    ///        If no bids exceed this price, the asset is returned to `seller`.
     function createAuction(
         address tokenContract,
         uint256 tokenId,
-        uint32 startTime, 
+        uint32 startTime,
         uint32 bidPeriod,
         uint32 revealPeriod,
         uint96 reservePrice
-    ) 
-        external 
-        nonReentrant
-    {
+    ) external nonReentrant {
         Auction storage auction = auctions[tokenContract][tokenId];
 
         if (startTime == 0) {
@@ -137,7 +48,7 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         if (revealPeriod < 1 hours) {
             revert RevealPeriodTooShortError(revealPeriod);
         }
-        
+
         auction.seller = msg.sender;
         auction.startTime = startTime;
         auction.endOfBiddingPeriod = startTime + bidPeriod;
@@ -145,7 +56,7 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         // Reset
         auction.numUnrevealedBids = 0;
         // Both highest and second-highest bid are set to the reserve price.
-        // Any winning bid must be at least this price, and the winner will 
+        // Any winning bid must be at least this price, and the winner will
         // pay at least this price.
         auction.highestBid = reservePrice;
         auction.secondHighestBid = reservePrice;
@@ -167,23 +78,11 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         );
     }
 
-    /// @notice Commits to a bid on an item being auctioned. If a bid was
-    ///         previously committed to, overwrites the previous commitment.
-    ///         Value attached to this call is used as collateral for the bid.
-    /// @param tokenContract The address of the ERC721 contract for the asset
-    ///        being auctioned.
-    /// @param tokenId The ERC721 token ID of the asset being auctioned.
-    /// @param commitment The commitment to the bid, computed as
-    ///        `bytes20(keccak256(abi.encode(nonce, bidValue)))`.
     function commitBid(
-        address tokenContract, 
-        uint256 tokenId, 
+        address tokenContract,
+        uint256 tokenId,
         bytes20 commitment
-    )
-        external
-        payable
-        nonReentrant
-    {
+    ) external payable nonReentrant {
         if (commitment == bytes20(0)) {
             revert ZeroCommitmentError();
         }
@@ -191,14 +90,16 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         Auction storage auction = auctions[tokenContract][tokenId];
 
         if (
-            block.timestamp < auction.startTime || 
+            block.timestamp < auction.startTime ||
             block.timestamp > auction.endOfBiddingPeriod
         ) {
             revert NotInBidPeriodError();
         }
 
         uint64 auctionIndex = auction.index;
-        Bid storage bid = bids[tokenContract][tokenId][auctionIndex][msg.sender];
+        Bid storage bid = bids[tokenContract][tokenId][auctionIndex][
+            msg.sender
+        ];
         // If this is the bidder's first commitment, increment `numUnrevealedBids`.
         if (bid.commitment == bytes20(0)) {
             auction.numUnrevealedBids++;
@@ -209,21 +110,12 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         }
     }
 
-    /// @notice Reveals the value of a bid that was previously committed to. 
-    /// @param tokenContract The address of the ERC721 contract for the asset
-    ///        being auctioned.
-    /// @param tokenId The ERC721 token ID of the asset being auctioned.
-    /// @param bidValue The value of the bid.
-    /// @param nonce The random input used to obfuscate the commitment.
     function revealBid(
         address tokenContract,
         uint256 tokenId,
         uint96 bidValue,
         bytes32 nonce
-    )
-        external
-        nonReentrant
-    {
+    ) external nonReentrant {
         Auction storage auction = auctions[tokenContract][tokenId];
 
         if (
@@ -234,7 +126,9 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         }
 
         uint64 auctionIndex = auction.index;
-        Bid storage bid = bids[tokenContract][tokenId][auctionIndex][msg.sender];
+        Bid storage bid = bids[tokenContract][tokenId][auctionIndex][
+            msg.sender
+        ];
 
         // Check that the opening is valid
         bytes20 bidHash = bytes20(keccak256(abi.encode(nonce, bidValue)));
@@ -277,14 +171,7 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
             );
         }
     }
-    
-    /// @notice Ends an active auction. Can only end an auction if the bid reveal
-    ///         phase is over, or if all bids have been revealed. Disburses the auction
-    ///         proceeds to the seller. Transfers the auctioned asset to the winning
-    ///         bidder and returns any excess collateral. If no bidder exceeded the
-    ///         auction's reserve price, returns the asset to the seller.
-    /// @param tokenContract The address of the ERC721 contract for the asset auctioned.
-    /// @param tokenId The ERC721 token ID of the asset auctioned.
+
     function endAuction(address tokenContract, uint256 tokenId)
         external
         nonReentrant
@@ -306,15 +193,25 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         address highestBidder = auction.highestBidder;
         if (highestBidder == address(0)) {
             // No winner, return asset to seller.
-            ERC721(tokenContract).safeTransferFrom(address(this), auction.seller, tokenId);
+            ERC721(tokenContract).safeTransferFrom(
+                address(this),
+                auction.seller,
+                tokenId
+            );
         } else {
             // Transfer auctioned asset to highest bidder
-            ERC721(tokenContract).safeTransferFrom(address(this), highestBidder, tokenId);
+            ERC721(tokenContract).safeTransferFrom(
+                address(this),
+                highestBidder,
+                tokenId
+            );
             uint96 secondHighestBid = auction.secondHighestBid;
             auction.seller.safeTransferETH(secondHighestBid);
 
             // Return excess collateral
-            Bid storage bid = bids[tokenContract][tokenId][auction.index][highestBidder];
+            Bid storage bid = bids[tokenContract][tokenId][auction.index][
+                highestBidder
+            ];
             uint96 collateral = bid.collateral;
             bid.collateral = 0;
             if (collateral - secondHighestBid != 0) {
@@ -323,36 +220,29 @@ contract OverCollateralizedAuction is IOverCollateralizedAuctionErrors, Reentran
         }
     }
 
-    /// @notice Withdraws collateral. Bidder must have opened their bid commitment
-    ///         and cannot be in the running to win the auction.
-    /// @param tokenContract The address of the ERC721 contract for the asset
-    ///        that was auctioned.
-    /// @param tokenId The ERC721 token ID of the asset that was auctioned.
-    /// @param auctionIndex The index of the auction that was being bid on.
     function withdrawCollateral(
         address tokenContract,
         uint256 tokenId,
         uint64 auctionIndex
-    )
-        external
-        nonReentrant        
-    {
+    ) external nonReentrant {
         Auction storage auction = auctions[tokenContract][tokenId];
         uint64 currentAuctionIndex = auction.index;
         if (auctionIndex > currentAuctionIndex) {
             revert InvalidAuctionIndexError(auctionIndex);
         }
 
-        Bid storage bid = bids[tokenContract][tokenId][auctionIndex][msg.sender];
+        Bid storage bid = bids[tokenContract][tokenId][auctionIndex][
+            msg.sender
+        ];
         if (bid.commitment != bytes20(0)) {
             revert UnrevealedBidError();
         }
 
         if (auctionIndex == currentAuctionIndex) {
-            // If bidder has revealed their bid and is not currently in the 
+            // If bidder has revealed their bid and is not currently in the
             // running to win the auction, they can withdraw their collateral.
             if (msg.sender == auction.highestBidder) {
-                revert CannotWithdrawError();    
+                revert CannotWithdrawError();
             }
         }
         // Return collateral
